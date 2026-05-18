ea9cfe6eb686d82a216c8cd13df6ade1aeaa7393
barracuda <dimitrijchannel@gmail.com>
2026-05-18 15:29:00 +0300

    Add more dependencies
    - Refactored compose.yaml file to include more complicated kafka topology and added postgres container.
    - Added dev properties for dev profile
    - Added more required dependencies by hte project: docker compose, liquibase, kafka.
    
    Signed-off-by: barracuda <dimitrijchannel@gmail.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# JCampaign
+A toy application for businesses.
+
+# Functionality
+- Multi Tenancy. Each customer should have their own database for their data.
+- Store customer information.
+- Statistics and analytics of the stored information.
+- Member cards with point system and flexible configuration.
+- Newsletter functionality.
+- Allow configuring actions for events in the system(Kafka consumers working with configurable behavior).
--- src/main/java/com/barracuda/jcampaign/JCampaignApplication.java
+++ src/main/java/com/barracuda/jcampaign/JCampaignApplication.java
@@ -1,7 +1,20 @@
 package com.barracuda.jcampaign;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
 
 @SpringBootApplication
 public class JCampaignApplication {
