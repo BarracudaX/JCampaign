caa2f5c34a8e80fe00afa1d01e16b76d5ea188fa
barracuda <dimitrijchannel@gmail.com>
2026-05-18 15:31:23 +0300

    Add test profile and appropriate properties files
    Signed-off-by: barracuda <dimitrijchannel@gmail.com>

--- src/test/java/com/barracuda/jcampaign/JCampaignApplicationTest.java
+++ src/test/java/com/barracuda/jcampaign/JCampaignApplicationTest.java
@@ -3,10 +3,12 @@ package com.barracuda.jcampaign;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @Import(TestcontainersConfiguration.class)
+@ActiveProfiles("test")
 @SpringBootTest
-class JCampaignApplicationTests {
+class JCampaignApplicationTest {
 
     @Test
     void contextLoads() {
