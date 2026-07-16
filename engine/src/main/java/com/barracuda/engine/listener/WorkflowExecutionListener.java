e66881c072f4006d02f302ab0d4b124aff379c92
barracuda <dimitrijchannel@gmail.com>
2026-07-16 18:57:27 +0300

    Remove engine code from the repo to be moved into submodule instead
    Signed-off-by: barracuda <dimitrijchannel@gmail.com>

--- engine/src/main/java/com/barracuda/engine/listener/WorkflowExecutionListener.java
+++
@@ -1,9 +0,0 @@
-package com.barracuda.engine.listener;
-
-import com.barracuda.engine.event.WorkflowEvent;
-
-public interface WorkflowExecutionListener {
-
-    void event(WorkflowEvent event);
-
-}
